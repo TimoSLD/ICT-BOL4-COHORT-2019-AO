b1501ea16aea1a648c4c58a52e2e6e384770e708
timo lemmen <99058056@mydavinci.nl>
2019-09-11 13:44:20 +0200

    pizza calculator + labs

---
+++ week 1/lab 1/b1w2l1.js
@@ -0,0 +1,13 @@
+//Timo lemmen
+//applictaite- en media ontwikkelaar
+
+// begin hier je JavaScript commandos
+alert ('u moet 3 vragen beantwoorden'); //een pop-up bericht die iets zegt.
+
+var voornaam = prompt ('vul je voornaam in'); //een bericht waarin je je voornaam kan typen
+var achternaam  = prompt ('vul je achternaam in'); //een bericht waarin je je achternaam kan typen
+var leeftijd = prompt ('vul je leeftijd in'); //een bericht waarin je je leeftijd kan typen
+
+document.write ("mijn naam is " + " " + achternaam + "<br> en mijn leeftijd"+ leeftijd); //de antwoorden komen op de body van de website
+
+
