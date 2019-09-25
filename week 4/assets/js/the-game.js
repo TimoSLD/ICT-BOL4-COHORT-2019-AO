aafaabd29653efe839010d38e83890479bf44461
timo lemmen <99058056@mydavinci.nl>
2019-09-25 09:51:21 +0200

    the game

---
+++ week 4/assets/js/the-game.js
@@ -0,0 +1,48 @@
+{
+  alert("Welkom bij the-game!");
+}
+ 
+var naam = prompt("Wat is uw naam?");
+var naam = alert (' welkom ' +naam+ ', leuk dat je deze game speelt');
+
+function askQuestion() {
+  return prompt("What the name of the company that developed the javascript language?", "");
+}
+
+var vraag1 = prompt("je staat op het punt om te ontsnappen uit de gevangenis. \
+Je komt terecht op een punt dat je naar het bos of strand kan gaan\n1: ga het bos in.\n2:ga naar het strand.")
+if (vraag1 == 1){
+  alert("Goed, klik op OK/ENTER om verder te gaan")
+} else if (vraag1 == 2) {
+  alert("Er staan bewakers op het strand, je bent opgepakt!")
+location.reload(true);
+}
+
+if (vraag1 == 1)
+var vraag2 = prompt("je bent nu in het bos en je komt op een pad terecht die in tweeen splitst. je kan naar rechts en links.\n1: ga naar rechts.\n2:ga naar links")
+
+if (vraag2 == 1){
+  prompt ("goed")
+} else if (vraag2 == 2){
+  alert ("fout")
+  location.reload(true);
+}
+
+var vraag3 = prompt()
+
+ 
+
+
+
+
+    
+  
+  
+
+  
+  
+  
+
+
+
+
