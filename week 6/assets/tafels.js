0c68c46c3a321c141d9c1517300d32087112a5fa
timo lemmen <99058056@mydavinci.nl>
2019-10-08 13:29:29 +0200

    klaar

---
+++ week 6/assets/tafels.js
@@ -0,0 +1,8 @@
+console.log("start tafels");
+for (var i = 0; i <= 10; i++) {
+    console.log("start tafel van " + i);
+    for (var b = 1; b <= 10; b++) {
+        var ans = i * b;
+        console.log(i + " x " + b + " = " + ans);
+    }
+}
