b68a8a1afa616615169f6daa3afb173ef8da032c
timo lemmen <99058056@mydavinci.nl>
2019-11-01 12:06:26 +0100

    klaar met week 8

--- week 8/assets/js/Horeca.js
+++ week 8/assets/js/Horeca.js
@@ -7,6 +7,13 @@ const PRIJSBIER = 2.50
 const PRIJSWIJN = 4.80
 const PRIJSSNACKS8 = 4.85
 const PRIJSSNACKS16 = 6.99
+var totaalFris = 0
+var totaalBier = 0
+var totaalWijn = 0
+totaalPrijsSnacks16 = 0
+totaalPrijsSnacks8 = 0
+
+
 
 var keuze = prompt ("welke bestelling wilt u toevoegen? kies uit drankjes of snacks");
 if (keuze == "drankjes"){
@@ -23,18 +30,18 @@ if (keuze == "drankjes"){
     function plaatsBestelling (){
         var keuze = prompt ("Je kunt nu opgeven welk drankje je wilt bestellen. Je kunt intypen 'fris', 'bier' of 'wijn'");
             if (keuze == "fris"){
-                var aantalDrankjes = prompt ("Hoeveel fris wilt u toevoegen aan uw bestelling")
-                totaalPrijs += (PRIJSFRIS * aantalDrankjes)
+                var aantalDrankjes = parseInt(prompt ("Hoeveel fris wilt u toevoegen aan uw bestelling"))
+                totaalFris = (totaalFris + aantalDrankjes)
                 plaatsBestelling()
             }
             else if (keuze == "bier"){
-                var aantalDrankjes = prompt ("Hoeveel bier wilt u toevoegen aan uw bestelling")
-                totaalPrijs += (PRIJSBIER * aantalDrankjes)
+                var aantalDrankjes = parseInt(prompt ("Hoeveel bier wilt u toevoegen aan uw bestelling"))
+                totaalBier = (totaalBier + aantalDrankjes)
                 plaatsBestelling()
             }
             else if (keuze == "wijn"){
-                var aantalDrankjes = prompt ("Hoeveel wijn wilt u toevoegen aan uw bestelling")
-                totaalPrijs += (PRIJSWIJN * aantalDrankjes)
+                var aantalDrankjes = parseInt(prompt ("Hoeveel wijn wilt u toevoegen aan uw bestelling"))
+                totaalWijn = (totaalWijn + aantalDrankjes)
                 plaatsBestelling()
             }
             else if (keuze == "snacks"){
@@ -49,13 +56,13 @@ if (keuze == "drankjes"){
     function plaatsBestellingSnacks (){
             keuze = prompt ("Hoeveel bitterballen wilt u toevoegen, een schaal van 8 of 16?");
             if (keuze== "8"){
-                var  aantalSnacks = prompt ("Hoeveel bitterballenschalen van 8 stuks wilt u toevoegen aan uw bestelling")
-                totaalPrijsSnacks += (PRIJSSNACKS8 * aantalSnacks)
+                var  aantalSnacks = parseInt(prompt ("Hoeveel bitterballenschalen van 8 stuks wilt u toevoegen aan uw bestelling"))
+                totaalPrijsSnacks8 = (PRIJSSNACKS8 + aantalSnacks)
                 plaatsBestellingSnacks()
             }
             else if (keuze == "16"){
-                var aantalSnacks = prompt ("Hoeveel bitterballenschalen van 16 stuks wilt u toevoegen aan uw bestelling")
-                totaalPrijsSnacks += (PRIJSSNACKS16 * aantalSnacks)
+                var aantalSnacks = parseInt(prompt ("Hoeveel bitterballenschalen van 16 stuks wilt u toevoegen aan uw bestelling"))
+                totaalPrijsSnacks16 = (PRIJSSNACKS16 + aantalSnacks)
                 plaatsBestellingSnacks()
             
             }
@@ -70,5 +77,12 @@ if (keuze == "drankjes"){
         }
             
     function afrekenen (){
-        document.write ("totaal bedrag: €"+totaalPrijs.toFixed(2)+totaalPrijsSnacks.toFixed(2))
+        document.write(totaalFris + " Drankjes Fris - €" + PRIJSFRIS.toFixed(2) * totaalFris + "<br>")
+        document.write(totaalBier + " Drankjes Bier - €" + PRIJSBIER.toFixed(2) * totaalBier + "<br>")
+        document.write(totaalWijn + " Drankjes Wijn - €" + PRIJSWIJN.toFixed(2) * totaalWijn + "<br>")
+        document.write(totaalPrijsSnacks8 + " gehaktballenschaal 8 - €" + PRIJSSNACKS8.toFixed(2) * totaalPrijsSnacks8  + "<br>")
+        document.write(totaalPrijsSnacks16 + " gehaktballenschaal 16 - €" + PRIJSSNACKS16.toFixed(2) * totaalPrijsSnacks16 + "<br>")
+        document.write("Sub totaal: €" + PRIJSFRIS.toFixed(2) * totaalFris + PRIJSBIER.toFixed(2) * totaalBier + PRIJSWIJN.toFixed(2) 
+        * totaalWijn + PRIJSSNACKS8.toFixed(2) * totaalPrijsSnacks8  + PRIJSSNACKS16.toFixed(2) * totaalPrijsSnacks16)
     }
+    
